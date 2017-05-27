53cf0028d3591978bdb63ece32997daefe214d29
oleh <komolebo@gmail.com>
2017-05-27 06:26:05 +0300

    Remove noise reduction

--- src/filters/VAD.py
+++ src/filters/VAD.py
@@ -4,7 +4,6 @@
 # Date: Tue Jun 10 15:17:26 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
-from noisered import NoiseReduction
 from silence import remove_silence
 from ltsd import LTSD_VAD
 
@@ -14,22 +13,15 @@ import numpy as np
 class VAD(object):
     def __init__(self):
         self.initted = False
-        self.nr = NoiseReduction()
         self.ltsd = LTSD_VAD()
 
     def init_noise(self, fs, signal):
         self.initted = True
-        self.nr.init_noise(fs, signal)
         self.ltsd.init_params_by_noise(fs, signal)
-        # nred = self.nr.filter(fs, signal)
-        #self.ltsd.init_params_by_noise(fs, nred)
 
     def filter(self, fs, signal):
         if not self.initted:
-            raise "NoiseFilter Not Initialized"
-        # nred = self.nr.filter(fs, signal)
-        #        removed = remove_silence(fs, nred)
-        #        self.ltsd.plot_ltsd(fs, nred)
+            raise Exception("NoiseFilter Not Initialized")
         filtered, intervals = self.ltsd.filter(signal)
         return filtered, intervals
 
